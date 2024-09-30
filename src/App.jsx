89b16088c73007726f8140683ec9eb3d76d60669
George <george.alexander.saav@gmail.com>
2024-09-30 09:58:43 +0200

    Making progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { useState } from "react"
 function App() {
 
   const [display1, setDisplay1] = useState("0")
+  const [display2, setDisplay2] = useState("0")
+  const [operation, setOperation] = useState("+")
+
 
   const updateDisplay1 = (number) => {
     if (display1 === "0") {
@@ -16,49 +19,72 @@ function App() {
     console.log(parseInt(display1))
   }
 
+  const clearDisplay1 = () => {
+    setDisplay1("0")
+  }
+
+  const updateDisplay2 = (number) => {
+    if (display2 === "0") {
+      setDisplay2(number)
+    }
+    else {
+      setDisplay2((display2) => display2.toString() + number.toString())
+    }
+    console.log(display2)
+    console.log(parseInt(display2))
+  }
+
+  const clearDisplay2 = () => {
+    setDisplay2("0")
+  }
+
+  const updateOperation = (operator) => {
+    setOperation(operator);
+  }
+
   return (
     <div className="calculator">
       <div className="panel">
         <p>{display1}</p>
         <div className="numbers">
           <button onClick={() => updateDisplay1(1)}>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => updateDisplay1(2)}>2</button>
+          <button onClick={() => updateDisplay1(3)}>3</button>
+          <button onClick={() => updateDisplay1(4)}>4</button>
+          <button onClick={() => updateDisplay1(5)}>5</button>
+          <button onClick={() => updateDisplay1(6)}>6</button>
+          <button onClick={() => updateDisplay1(7)}>7</button>
+          <button onClick={() => updateDisplay1(8)}>8</button>
+          <button onClick={() => updateDisplay1(9)}>9</button>
+          <button onClick={() => updateDisplay1(0)}>0</button>
+          <button onClick={() => clearDisplay1()}>Clear</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>+</p>
+        <p>{operation}</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => updateOperation("+")}>+</button>
+          <button onClick={() => updateOperation("-")}>-</button>
+          <button onClick={() => updateOperation("*")}>*</button>
+          <button onClick={() => updateOperation("÷")}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{display2}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          <button onClick={() => updateDisplay2(1)}>1</button>
+          <button onClick={() => updateDisplay2(2)}>2</button>
+          <button onClick={() => updateDisplay2(3)}>3</button>
+          <button onClick={() => updateDisplay2(4)}>4</button>
+          <button onClick={() => updateDisplay2(5)}>5</button>
+          <button onClick={() => updateDisplay2(6)}>6</button>
+          <button onClick={() => updateDisplay2(7)}>7</button>
+          <button onClick={() => updateDisplay2(8)}>8</button>
+          <button onClick={() => updateDisplay2(9)}>9</button>
+          <button onClick={() => updateDisplay2(0)}>0</button>
+          <button onClick={() => clearDisplay2()}>Clear</button>
         </div>
       </div>
       <div className="panel answer">
